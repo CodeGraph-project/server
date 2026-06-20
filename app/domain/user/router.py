680cbc09130f415fe14d24a0306c8b8f5fd0a196
Siu0901 <trip0901@naver.com>
2026-06-20 15:11:11 +0900

    feat: 유저 api 개발
    자기 정보, 유저 검색

--- app/domain/user/router.py
+++ app/domain/user/router.py
@@ -1,21 +1,24 @@
 from fastapi import APIRouter, Query
-from app.domain.user.dependencies import CurrentUser, UserRepositoryDep, UserServiceDep
-from app.domain.user.schemas import UserResponse, UserSearchItem
+from app.domain.user.dependencies import CurrentUser, UserServiceDep
+from app.domain.user.schemas import UserSearchItem, MyProfileResponse
 
-user_router = APIRouter(prefix="/user", tags=["user"])
-
-# 지금 project, scrap쪽이 다 안된 상태여서, 일단 대충 틀만 짜둠. user 도메인 좀 많이 바뀔수도
-# 그리고 보니까 user 테이블에 유저 깃허브 링크가 없더라. 나중에 마이그레이션해서 수정해야 할 듯
-
-@user_router.get("/me", response_model=UserResponse)
-async def get_me(user: CurrentUser):
-    return user
+user_router = APIRouter(prefix="/users", tags=["users"])
 
 
-@user_router.get("/search", response_model=list[UserSearchItem])
+@user_router.get("/me", response_model=MyProfileResponse)
+async def get_me(user: CurrentUser, service: UserServiceDep):
+    stats = await service.get_stats(user.id)
+    return MyProfileResponse(
+        **user.model_dump(),
+        stats=stats
+    )
+
+
+@user_router.get("/search", response_model=UserSearchItem)
 async def search_user(
         user: CurrentUser,
         service: UserServiceDep,
-        q: str = Query(..., min_length=1, description="유저 검색어"),
+        q: str = Query(..., min_length=1),
+        exclude: list[int] = Query(default=[])
 ):
-    return await service.search_user(q)
+    return await service.search(q=q, exclude=exclude)
